1cd2bd092bca8c407118149d0bb2638584950c6a
Zanda <zandacw@gmail.com>
2024-07-10 21:14:20 +0300

    added python and zig lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -567,7 +567,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -577,7 +577,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
+        zls = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
